320187537b0eca54a65d58c6ae40a969bed2e335
Norn3 <1430835578@qq.com>
2023-07-31 07:13:18 +0800

    feat(add navbar component, improved sidebar): based on sideBar add navBar, improved sideBar overflow

--- src/components/navBar/navBar.vue
+++ src/components/navBar/navBar.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import $ from 'jquery';
 import { onMounted, ref, nextTick, reactive } from 'vue';
-import './sideBar.scss';
+import './navBar.scss';
 
 // 通过defineProps接收父组件的值
 const father = defineProps({
@@ -10,41 +10,38 @@ const father = defineProps({
     type: Number, // 数据类型
     default: 0, // 未传值时的默认值
   },
-  listItem: {
+  navItem: {
     type: Array,
-    default: () => ['Item1', 'Item2', 'Item3', 'Item4'],
+    default: () => [
+      'Item11568468796/8746844654134854135',
+      'Item2',
+      'Item3',
+      'Item4',
+    ],
   },
 });
-const listItem = reactive(father.listItem);
-// 列表项，可以通过导入实现
-// const listItem = ref(['Item1','Item2','Item3','Item4']);
-// let current = 0;
-
-// const List = ref<HTMLElement>();
+const navItem = reactive(father.navItem);
 
 const createItem = () => {
   // 使用 $() 将目标元素包装为 jQuery 对象
-  const $List = $('.List');
+  const $nav = $('.nav');
 
-  listItem.forEach((element) => {
+  navItem.forEach((element) => {
     // 创建新的子元素并设置其内容
     const $li = $('<li>').text(String(element));
-    // $li.attr('id','"listItem'+ (father.itemNum - current) +'"');
-    $li.addClass('listItem');
+    $li.addClass('navItem');
     // 将新创建的子元素添加为目标元素的子节点
-    $List.append($li);
+    $nav.append($li);
   });
 };
 
 onMounted(async () => {
-  // 等待页面加载结束，再调用createItem创建列表项
   await nextTick();
   createItem();
-  // console.log(father.itemNum);
 });
 </script>
 
 <template>
-  <ul class="List" ref="List"></ul>
+  <ul class="nav" ref="nav"></ul>
 </template>
 <style lang="scss" scoped></style>
--- src/views/PlayView.vue
+++ src/views/PlayView.vue
@@ -5,6 +5,7 @@ import { reactive, ref } from 'vue';
 import { useRouter } from 'vue-router';
 
 import sideBar from '../components/sideBar/sideBar.vue';
+import navBar from '../components/navBar/navBar.vue';
 
 // const route = useRoute()
 $(document).on('click', '#title', () => {
@@ -20,7 +21,7 @@ const jumpPage = () => {
 
 const itemNum = ref(12);
 const listItem = reactive([
-  'Item1',
+  'Item1468854638/763548484854',
   'Item2',
   'Item3',
   'Item4',
@@ -33,7 +34,9 @@ const listItem = reactive([
   <!-- <h1 id="title">PlayView</h1> -->
   <header></header>
   <article>
-    <nav></nav>
+    <nav>
+      <nav-bar></nav-bar>
+    </nav>
     <aside>
       <side-bar :itemNum="itemNum" :listItem="listItem"></side-bar>
     </aside>
