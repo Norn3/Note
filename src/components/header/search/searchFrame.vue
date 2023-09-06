2b7ab215883130dc1013bb62633951e2dbcaab9b
Norn3 <1430835578@qq.com>
2023-09-06 11:41:00 +0800

    feat(add searchframe in header): add searchFrame component in header

---
+++ src/components/header/search/searchFrame.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+import './searchFrame.scss';
+
+// defineProps<{ keyword: string }>()
+
+const keyword = ref<string | null>('');
+</script>
+
+<template>
+  <div id="search" class="search">
+    <input type="search" placeholder="search" v-model="keyword" />
+  </div>
+</template>
