008b0bb81193607925f10d32f50038775c0a45a3
Norn3 <1430835578@qq.com>
2023-09-08 13:55:46 +0800

    refactor(rearrange total routers): rearrange total routers

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,17 +1,59 @@
 import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router';
-import HomeView from '../views/HomeView.vue'
+import HomeView from '../views/home/HomeView.vue'
 
 // 配置信息中需要页面的相关配置，即路径代表的组件，页面对应关系
 const routes: Array<RouteRecordRaw> = [
 	{
 		path: '/',          // 通过什么路径来访问这个组件
 		name: 'home',
-		component: HomeView                // 路径对应的组件
+		component: HomeView,                // 路径对应的组件
+		children:[
+			{
+				path: 'homeRecommend',
+				name: 'recommend',
+				component: () => import('../views/home/HomeRecommendView.vue'),    
+			},
+			{
+				path: 'homePlayList',
+				name: 'playList',
+				component: () => import('../views/home/HomePlayListView.vue'),    
+			},
+			{
+				path: 'homeRank',
+				name: 'rank',
+				component: () => import('../views/home/HomeRankView.vue'),    
+			},
+		]
 	},
 	{
 		path: '/play',
 		name: 'play',
-		component: () => import('../views/PlayView.vue')    // 异步加载     
+		component: () => import('../views/PlayListView.vue'),    // 异步加载     
+		children: [
+			
+		]
+	},
+	{
+		path: '/account',
+		name: 'account',
+		component: () => import('../views/account/accountHomeView.vue'),
+		children:[
+			{
+				path: 'accountHome',
+				name: 'accountHome',
+				component: () => import('../views/account/accountHomeView.vue'),	
+			},
+			{
+				path: 'message',
+				name: 'accountMessage',
+				component: () => import('../views/account/accountMessageView.vue'),	
+			},
+			{
+				path: 'setting',
+				name: 'accountSetting',
+				component: () => import('../views/account/accountSettingView.vue'),	
+			},
+		]
 	}
 ]
 
