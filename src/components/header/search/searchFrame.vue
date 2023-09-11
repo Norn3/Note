da42c2d9b1e0f5f36ba6a9007be7515e3f3ee48b
Norn3 <1430835578@qq.com>
2023-09-11 19:34:09 +0800

    feat(import dev-ui for components): import dev-ui, and refactored searchFrame component
    import dev-ui, and refactored searchFrame component for search autocompletion
    
    BREAKING CHANGE: import dev-ui for components

--- src/components/header/search/searchFrame.vue
+++ src/components/header/search/searchFrame.vue
@@ -1,15 +1,130 @@
 <script setup lang="ts">
 import { ref } from 'vue';
+import { get } from '../../../axios/insatance';
 
 import './searchFrame.scss';
 
-// defineProps<{ keyword: string }>()
+const value = ref('');
+const allowEmptyValueSearch = ref(false);
 
-const keyword = ref<string | null>('');
+interface SourceItemObj {
+  category: string;
+  name: string;
+  index: number;
+}
+type SourceType = Array<string> | Array<SourceItemObj>;
+const mySource = ref<SourceType>([
+  {
+    category: 'none',
+    name: 'none',
+    index: 0,
+  },
+]);
+const formatter = (item: SourceItemObj) => {
+  if (item.index == 0) return `${item.category}：${item.name}`;
+  else return `\u3000\u3000\u3000${item.name}`;
+};
+//trem：input输入内容
+const searchFn = async (trem: string) => {
+  await get<any>(`/search/suggest?keywords=${trem}`)
+    .then((song) => {
+      // 处理返回的用户数据
+      let arr: Array<SourceItemObj> = [];
+      let id = 0;
+
+      if (song.songs) {
+        song.songs.forEach((element: SourceItemObj) => {
+          let item: SourceItemObj = {
+            category: '单曲',
+            name: element.name,
+            index: id++,
+          };
+          arr.push(item);
+        });
+      }
+
+      if (song.artists) {
+        id = 0;
+        song.artists.forEach((element: SourceItemObj) => {
+          let item: SourceItemObj = {
+            category: '歌手',
+            name: element.name,
+            index: id++,
+          };
+          arr.push(item);
+        });
+      }
+
+      if (song.albums) {
+        id = 0;
+        song.albums.forEach((element: SourceItemObj) => {
+          let item: SourceItemObj = {
+            category: '专辑',
+            name: element.name,
+            index: id++,
+          };
+          arr.push(item);
+        });
+      }
+
+      if (song.playlists) {
+        id = 0;
+        song.playlists.forEach((element: SourceItemObj) => {
+          let item: SourceItemObj = {
+            category: '歌单',
+            name: element.name,
+            index: id++,
+          };
+          arr.push(item);
+        });
+      }
+      console.log(arr);
+      mySource.value = arr;
+    })
+    .catch((error) => {
+      // 处理请求错误
+      console.log(error);
+    });
+  return mySource.value;
+};
+const selectValue = (e: Event) => {
+  get<any>(`/search/suggest?keywords=${e}`)
+    .then((song) => {
+      // 处理返回的用户数据
+      console.log(song);
+    })
+    .catch((error) => {
+      // 处理请求错误
+      console.log(error);
+    });
+};
+const transInputFocusEmit = () => {
+  // console.log('transInputFocusEmit');
+};
+const position = ref(['bottom']);
 </script>
 
 <template>
   <div id="search" class="search">
-    <input type="search" placeholder="search" v-model="keyword" />
+    <d-auto-complete
+      :delay="500"
+      :formatter="formatter"
+      :source="mySource"
+      :search-fn="searchFn"
+      is-searching
+      v-model="value"
+      :allow-empty-value-search="allowEmptyValueSearch"
+      :select-value="selectValue"
+      :trans-input-focus-emit="transInputFocusEmit"
+      :position="position"
+      :width="200"
+      :append-to-body="false"
+    >
+      <template #searching="slotProps">
+        <div>
+          {{ `searching: ${slotProps}` }}
+        </div>
+      </template>
+    </d-auto-complete>
   </div>
 </template>
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,11 @@ import './style.css'
 import App from './App.vue'
 import router from "./router"
 import $ from 'jquery'
+import DevUI from 'vue-devui';
+import 'vue-devui/style.css';
+import '@devui-design/icons/icomoon/devui-icon.css';
+import { ThemeServiceInit, infinityTheme } from 'devui-theme';
 
+ThemeServiceInit({ infinityTheme }, 'infinityTheme');
 
-createApp(App).use(router).use($).mount('#app')
+createApp(App).use(router).use($).use(DevUI).mount('#app')
