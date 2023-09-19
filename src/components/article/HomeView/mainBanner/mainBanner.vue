51161aef025a2cd0a45fdb984608e4148b29ae66
Norn3 <1430835578@qq.com>
2023-09-19 18:29:19 +0800

    feat(put pictures on banner component): put pictures on banner component

--- src/components/article/HomeView/mainBanner/mainBanner.vue
+++ src/components/article/HomeView/mainBanner/mainBanner.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import $ from 'jquery';
-import { nextTick, onMounted, ref } from 'vue';
 import './mainBanner.scss';
 
 import { get } from '../../../../axios/insatance';
@@ -25,97 +24,45 @@ import 'swiper/scss/scrollbar';
 import 'swiper/scss/a11y';
 
 const onSwiper = (swiper: any) => {
-  console.log(swiper);
+  get<any>('/banner')
+    .then((response) => {
+      // 处理返回的用户数据
+      console.log('请求成功');
+      pictureList = response.banners;
+      console.log(pictureList);
+      let id = 0;
+      pictureList.forEach((element) => {
+        let $swiperslide = $(`#mySlide${id++} img`);
+        $swiperslide.attr('src', `${element.imageUrl}`);
+        $swiperslide.attr('url', `${element.url}`);
+      });
+    })
+    .catch((error) => {
+      // 处理请求错误
+      console.log('请求失败');
+      console.log(error);
+    });
 };
 const onSlideChange = () => {
-  console.log('slide change');
+  // console.log('slide change');
 };
 
+$(document).on('click', '[id^="mySlide"]', (e) => {
+  // e.target是DOM对象，只能用getAttribute而非attr方法
+  const url = e.target.getAttribute('url');
+  if (url != null && url != 'null') {
+    window.open(url, '_blank');
+  }
+});
+
 const modules = [Navigation, Pagination, Scrollbar, A11y, Autoplay];
-// let pictureList: Array<string>;
-
-// console.log('开始发送请求');
-
-// get<any>("/banner")
-//   .then((picture) => {
-//     // 处理返回的用户数据
-//     console.log('请求成功');
-//     console.log(picture);
-//     pictureList = picture;
-//   })
-//   .catch((error) => {
-//     // 处理请求错误
-//     console.log('请求失败');
-//     console.log(error);
-//   });
-
-// const createPicture = () => {
-//   const $pictureComponent = $('#picture');
-//   $pictureComponent.removeClass('loading');
-//   let id = 0;
-//   pictureList.forEach((element) => {
-//     // 创建新的子元素并设置其内容
-//     const $li = $('<li>').css('background-image',element)
-//     $(document).on('click',`#dot${id}`, () =>{
-
-//     })
-//     // $li.attr('id','"listItem'+ (father.itemNum - current) +'"');
-//     // $li.addClass('listItem');
-//     // 将新创建的子元素添加为目标元素的子节点
-//     $pictureComponent.append($li);
-//   });
-// }
-
-// let id = 0;
-
-// const circulate = setInterval(() => {
-//   $(`#dot${id++}`).removeClass('check');
-//   $(`#dot${id}`).addClass('check');
-//   $('#picture')[0].style.backgroundImage = pictureList[id];
-
-// },3000)
-
-// $(document).on('click','.dot',(event) => {
-//   clearInterval(circulate);
-//   $(`#dot${id}`).removeClass('check');
-//   event.target.addClass('check');
-
-// })
-
-// async function getBanner(){
-// 	const res = await get<any>("/banner");
-//   console.log(res);
-// }
-
-// onMounted(async () => {
-//   await nextTick();
-//   getBanner();
-//   // createPicture();
-// });
+let pictureList: Array<any>;
 </script>
 
 <template>
   <div id="mainBanner" class="mainBanner">
-    <!-- <div id="left" class="left"></div>
-    <div id="banner" class="banner">
-      <div id="picture" class="picture loading"></div>
-      <div id="dots" class="dots">
-        <ul>
-          <li id="dot0" class="dot check"></li>
-          <li id="dot1" class="dot"></li>
-          <li id="dot2" class="dot"></li>
-          <li id="dot3" class="dot"></li>
-          <li id="dot4" class="dot"></li>
-          <li id="dot5" class="dot"></li>
-          <li id="dot6" class="dot"></li>
-          <li id="dot7" class="dot"></li>
-          <li id="dot8" class="dot"></li>
-          <li id="dot9" class="dot"></li>
-        </ul>
-      </div>
-    </div>
-    <div id="right" class="right"></div> -->
     <swiper
+      id="mySwiper"
       :modules="modules"
       :slides-per-view="1"
       :space-between="50"
@@ -130,12 +77,13 @@ const modules = [Navigation, Pagination, Scrollbar, A11y, Autoplay];
         disableOnInteraction: false,
       }"
     >
-      <swiper-slide>Slide 1</swiper-slide>
-      <swiper-slide>Slide 2</swiper-slide>
-      <swiper-slide>Slide 3</swiper-slide>
-      <swiper-slide>Slide 4</swiper-slide>
-      <swiper-slide>Slide 5</swiper-slide>
-      <swiper-slide>Slide 6</swiper-slide>
+      <swiper-slide id="mySlide0"><img alt="Slide0" /></swiper-slide>
+      <swiper-slide id="mySlide1"><img alt="Slide1" /></swiper-slide>
+      <swiper-slide id="mySlide2"><img alt="Slide2" /></swiper-slide>
+      <swiper-slide id="mySlide4"><img alt="Slide4" /></swiper-slide>
+      <swiper-slide id="mySlide5"><img alt="Slide5" /></swiper-slide>
+      <swiper-slide id="mySlide6"><img alt="Slide6" /></swiper-slide>
+      <swiper-slide id="mySlide7"><img alt="Slide7" /></swiper-slide>
       ...
     </swiper>
   </div>
