e88ac3b7156de6d63104178f6e03e87a0223d56c
Norn3 <1430835578@qq.com>
2023-11-10 16:23:14 +0800

    feat(improve sidebar components into sidebar and sidebaritem components): improve sideBar components
    improve sideBar components into sideBar and sideBarItem components

--- src/components/article/sideBar/sideBar.vue
+++ src/components/article/sideBar/sideBar.vue
@@ -1,7 +1,23 @@
+<template>
+  <div id="sideBar" class="sideBar">
+    <div class="sideBarTitle"><h3>创建的歌单</h3></div>
+    <ul id="create" class="List"></ul>
+    <div class="sideBarTitle"><h3>收藏的歌单</h3></div>
+    <ul id="like" class="List"></ul>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
+
 <script setup lang="ts">
 import $ from 'jquery';
-import { onMounted, ref, nextTick, reactive, watch } from 'vue';
+import { onMounted, ref, nextTick, reactive, watch, h, render } from 'vue';
+
 import './sideBar.scss';
+
+import SideBarItem from './sideBarItem/sideBarItem.vue';
+
+import PlaylistItem from '../../../class/PlaylistItemClass';
 
 // 通过defineProps接收父组件的值
 const father = defineProps({
@@ -11,11 +27,13 @@ const father = defineProps({
     default: 0, // 未传值时的默认值
   },
   listItem: {
-    type: Array<number>,
-    default: () => [1, 2, 3, 4],
+    type: Array<PlaylistItem>,
+    default: () => [],
   },
 });
 const listItem = reactive(father.listItem);
+const uid = 571024254;
+
 watch(listItem, (newListItem) => {
   createItem(newListItem);
 });
@@ -25,29 +43,62 @@ watch(listItem, (newListItem) => {
 
 // const List = ref<HTMLElement>();
 
-const createItem = (listItem: Array<number>) => {
+const createItem = (listItem: Array<PlaylistItem>) => {
   // 使用 $() 将目标元素包装为 jQuery 对象
-  const $List = $('.List');
+  let $List = $('#create');
 
   listItem.forEach((element) => {
     // 创建新的子元素并设置其内容
+    // console.log(element);
 
-    const $li = $('<li>').text(String(element));
+    if (element.userId != uid) {
+      $List = $('#like');
+    }
+    const $li = $('<li>');
     // $li.attr('id','"listItem'+ (father.itemNum - current) +'"');
     $li.addClass('listItem');
     // 将新创建的子元素添加为目标元素的子节点
-    $List.append($li);
+    const li = $li[0];
+    $List.append(li);
+    render(h(SideBarItem), li);
+
+    $li.find('#name').text(String(element.name));
+    $li.find('#cover').attr('src', element.coverImgUrl);
+    $li.find('#number').text(String(element.trackCount) + '首');
   });
+  if ($('#create').children(':first') != undefined) {
+    $('#create').children(':first').addClass('check');
+  } else if ($('#like').children(':first') != undefined) {
+    $('#like').children(':first').addClass('check');
+  }
 };
+
+$(document).on('click', '.sideBarTitle', (element) => {
+  const $title = $(element.currentTarget);
+  const $h3 = $title.find('h3');
+  $h3.toggleClass('open');
+  const $list = $title.next();
+
+  $list.toggleClass('fold');
+});
+
+// 点击列表项
+$(document).on('click', '.listItem', (element) => {
+  // 取消之前选中项的选中样式
+  const $check = $('#create').find('.check')
+    ? $('#create').find('.check')
+    : $('#like').find('.check');
+  $check.removeClass('check');
+  // 给当前选中项添加选中样式
+  const $item = $(element.currentTarget);
+  $item.addClass('check');
+});
 
 onMounted(async () => {
   // 等待页面加载结束，再调用createItem创建列表项
   await nextTick();
+  // 加载时给第一项添加选中样式
+  $('#create')[0].classList.add('check');
   // console.log(father.itemNum);
 });
 </script>
-
-<template>
-  <ul class="List" ref="List"></ul>
-</template>
-<style lang="scss" scoped></style>
---
+++ src/components/article/sideBar/sideBarItem/sideBarItem.vue
@@ -0,0 +1,58 @@
+<script setup lang="ts">
+import $ from 'jquery';
+import { onMounted, ref, nextTick, reactive, watch } from 'vue';
+
+onMounted(async () => {
+  // 等待页面加载结束，再调用createItem创建列表项
+  await nextTick();
+  // console.log(father.itemNum);
+});
+</script>
+
+<template>
+  <div id="sideBarItem" class="sideBarItem">
+    <img src="" alt="" id="cover" class="cover" />
+    <div id="name" class="name"></div>
+    <div id="number" class="number"></div>
+  </div>
+</template>
+<style lang="scss" scoped>
+.sideBarItem {
+  width: 100%;
+  height: auto;
+  display: grid;
+  /*  声明列的宽度  */
+  grid-template-columns: 50px repeat(3, auto);
+  /*  声明行间距和列间距  */
+  grid-gap: 7px;
+  /*  声明行的高度  */
+  grid-template-rows: repeat(2, 1fr);
+  justify-items: start;
+
+  .cover {
+    width: 50px;
+    height: 50px;
+    grid-column: 1;
+    grid-row: 1 / 3;
+  }
+
+  .name {
+    grid-column: 2 / 5;
+    grid-row: 1;
+    max-width: 140px;
+    font-size: 1em;
+    overflow: hidden;
+    white-space: nowrap;
+    // 溢出显示省略号
+    text-overflow: ellipsis;
+  }
+
+  .number {
+    grid-column: 2 / 5;
+    grid-row: 2;
+    font-size: 0.7em;
+    color: gray;
+    white-space: nowrap;
+  }
+}
+</style>
