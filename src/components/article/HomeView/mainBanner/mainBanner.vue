85e3c5ad16d3eb4c1fe54dedf742bdbbbc699db3
Norn3 <1430835578@qq.com>
2023-11-17 16:29:35 +0800

    perf(improve mainbannner component loading speed): improve mainBannner component loading speed

--- src/components/article/HomeView/mainBanner/mainBanner.vue
+++ src/components/article/HomeView/mainBanner/mainBanner.vue
@@ -3,15 +3,10 @@ import $ from 'jquery';
 import './mainBanner.scss';
 
 import { get } from '../../../../axios/insatance';
+import { onBeforeMount, reactive } from 'vue';
 
 // import Swiper core and required modules
-import {
-  Navigation,
-  Pagination,
-  Scrollbar,
-  A11y,
-  Autoplay,
-} from 'swiper/modules';
+import { Navigation, Pagination, A11y, Autoplay } from 'swiper/modules';
 
 // Import Swiper Vue.js components
 import { Swiper, SwiperSlide } from 'swiper/vue';
@@ -22,25 +17,37 @@ import 'swiper/scss/navigation';
 import 'swiper/scss/pagination';
 import 'swiper/scss/a11y';
 
-const onSwiper = (swiper: any) => {
-  get<any>('/banner')
+interface Picture {
+  src: string;
+  url: string;
+}
+let pictureList: Array<Picture> = reactive([]);
+const getPicture = async () => {
+  await get<any>('/banner')
     .then((response) => {
       // 处理返回的用户数据
       console.log('请求成功');
-      pictureList = response.banners;
-      console.log(pictureList);
-      let id = 0;
-      pictureList.forEach((element) => {
-        let $swiperslide = $(`#mySlide${id++} img`);
-        $swiperslide.attr('src', `${element.imageUrl}`);
-        $swiperslide.attr('url', `${element.url}`);
+      console.log(response.banners);
+
+      response.banners.forEach((element: any) => {
+        const picture: Picture = { src: element.imageUrl, url: element.url };
+        pictureList.push(picture);
       });
+      console.log(pictureList);
     })
     .catch((error) => {
       // 处理请求错误
       console.log('请求失败');
       console.log(error);
     });
+};
+
+onBeforeMount(async () => {
+  await getPicture();
+});
+
+const onSwiper = (swiper: any) => {
+  // console.log('onSwiper');*
 };
 const onSlideChange = () => {
   // console.log('slide change');
@@ -49,13 +56,14 @@ const onSlideChange = () => {
 $(document).on('click', '[id^="mySlide"]', (e) => {
   // e.target是DOM对象，只能用getAttribute而非attr方法
   const url = e.target.getAttribute('url');
+  console.log(url);
+
   if (url != null && url != 'null') {
     window.open(url, '_blank');
   }
 });
 
 const modules = [Navigation, Pagination, A11y, Autoplay];
-let pictureList: Array<any>;
 </script>
 
 <template>
@@ -75,14 +83,13 @@ let pictureList: Array<any>;
         disableOnInteraction: false,
       }"
     >
-      <swiper-slide id="mySlide0"><img alt="Slide0" /></swiper-slide>
-      <swiper-slide id="mySlide1"><img alt="Slide1" /></swiper-slide>
-      <swiper-slide id="mySlide2"><img alt="Slide2" /></swiper-slide>
-      <swiper-slide id="mySlide4"><img alt="Slide4" /></swiper-slide>
-      <swiper-slide id="mySlide5"><img alt="Slide5" /></swiper-slide>
-      <swiper-slide id="mySlide6"><img alt="Slide6" /></swiper-slide>
-      <swiper-slide id="mySlide7"><img alt="Slide7" /></swiper-slide>
-      ...
+      <swiper-slide
+        id="mySlide0"
+        v-for="(picture, index) in pictureList"
+        :key="index"
+      >
+        <img alt="Slide0" :src="picture.src" :url="picture.url" />
+      </swiper-slide>
     </swiper>
   </div>
 </template>
