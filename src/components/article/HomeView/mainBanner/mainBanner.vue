134faece66c9f2a214dd15f27bb182a1c5d1729c
Norn3 <1430835578@qq.com>
2023-11-25 15:04:26 +0800

    refactor(update mainbanner component): update mainBanner component
    update mainBanner component

--- src/components/article/HomeView/mainBanner/mainBanner.vue
+++ src/components/article/HomeView/mainBanner/mainBanner.vue
@@ -1,3 +1,40 @@
+<template>
+  <div id="mainBanner" class="mainBanner">
+    <swiper
+      id="mySwiper"
+      :modules="modules"
+      :slides-per-view="1"
+      :space-between="50"
+      :navigation="true"
+      :rewind="true"
+      :pagination="{ clickable: true }"
+      @swiper="onSwiper"
+      @slideChange="onSlideChange"
+      :autoplay="{
+        delay: 3000,
+        disableOnInteraction: false,
+      }"
+    >
+      <swiper-slide
+        id="mySlide"
+        v-for="(picture, index) in pictureList"
+        :key="index"
+      >
+        <div
+          class="background"
+          :style="{ 'background-image': `url(${picture.src})` }"
+        >
+          <img
+            alt="Slide"
+            :src="picture.src"
+            :url="picture.url"
+            :targetId="picture.targetId"
+          />
+        </div>
+      </swiper-slide>
+    </swiper>
+  </div>
+</template>
 <script setup lang="ts">
 import $ from 'jquery';
 import './mainBanner.scss';
@@ -16,10 +53,18 @@ import 'swiper/scss';
 import 'swiper/scss/navigation';
 import 'swiper/scss/pagination';
 import 'swiper/scss/a11y';
+import router from '../../../../router';
 
 interface Picture {
   src: string;
-  url: string;
+  url: {
+    type: string;
+    default: null;
+  };
+  targetId: {
+    type: number;
+    default: 0;
+  };
 }
 let pictureList: Array<Picture> = reactive([]);
 const getPicture = async () => {
@@ -27,13 +72,16 @@ const getPicture = async () => {
     .then((response) => {
       // 处理返回的用户数据
       console.log('请求成功');
-      console.log(response.banners);
+      console.log(response);
 
       response.banners.forEach((element: any) => {
-        const picture: Picture = { src: element.imageUrl, url: element.url };
+        const picture: Picture = {
+          src: element.imageUrl,
+          url: element.url,
+          targetId: element.targetId,
+        };
         pictureList.push(picture);
       });
-      console.log(pictureList);
     })
     .catch((error) => {
       // 处理请求错误
@@ -53,43 +101,23 @@ const onSlideChange = () => {
   // console.log('slide change');
 };
 
-$(document).on('click', '[id^="mySlide"]', (e) => {
+$(document).on('click', '#mySlide', (e) => {
   // e.target是DOM对象，只能用getAttribute而非attr方法
   const url = e.target.getAttribute('url');
+  const target_id = e.target.getAttribute('targetId');
+
   console.log(url);
 
   if (url != null && url != 'null') {
     window.open(url, '_blank');
   }
+  // else if (target_id != null && target_id != 0){
+  //   router.push({
+  //   name: 'songInfo',
+  //   query: { pid: target_id },
+  // });
+  // }
 });
 
 const modules = [Navigation, Pagination, A11y, Autoplay];
 </script>
-
-<template>
-  <div id="mainBanner" class="mainBanner">
-    <swiper
-      id="mySwiper"
-      :modules="modules"
-      :slides-per-view="1"
-      :space-between="50"
-      :navigation="true"
-      :rewind="true"
-      :pagination="{ clickable: true }"
-      @swiper="onSwiper"
-      @slideChange="onSlideChange"
-      :autoplay="{
-        delay: 3000,
-        disableOnInteraction: false,
-      }"
-    >
-      <swiper-slide
-        id="mySlide0"
-        v-for="(picture, index) in pictureList"
-        :key="index"
-      >
-        <img alt="Slide0" :src="picture.src" :url="picture.url" />
-      </swiper-slide>
-    </swiper>
-  </div>
-</template>
