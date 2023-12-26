dfa34fc9e2100c817347a58d062edc7f5fdf7eec
Norn3 <1430835578@qq.com>
2023-12-26 17:47:45 +0800

    refactor(use pinia): use pinia

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import router from "./router"
+import { createPinia } from 'pinia'
 import $ from 'jquery'
 import DevUI from 'vue-devui';
 import 'vue-devui/style.css';
@@ -10,4 +11,6 @@ import { ThemeServiceInit, infinityTheme } from 'devui-theme';
 
 ThemeServiceInit({ infinityTheme }, 'infinityTheme');
 
-createApp(App).use(router).use($).use(DevUI).mount('#app')
+const pinia = createPinia()
+
+createApp(App).use(router).use(pinia).use($).use(DevUI).mount('#app')
