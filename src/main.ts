5e39c2dd68b228a81a996e954c30ff542ff795a6
Norn3 <1430835578@qq.com>
2024-02-17 14:45:24 +0800

    build(uninstall devui): uninstall devui because of the searchframe

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,7 @@ import App from './App.vue'
 import router from "./router"
 import { createPinia } from 'pinia'
 import $ from 'jquery'
-import DevUI from 'vue-devui';
-import 'vue-devui/style.css';
-import '@devui-design/icons/icomoon/devui-icon.css';
-import { ThemeServiceInit, infinityTheme } from 'devui-theme';
-
-ThemeServiceInit({ infinityTheme }, 'infinityTheme');
 
 const pinia = createPinia()
 
-createApp(App).use(router).use(pinia).use($).use(DevUI).mount('#app')
+createApp(App).use(router).use(pinia).use($).mount('#app')
---
+++ vite.config.ts.timestamp-1705562528576-eb534877c1ff2.mjs
@@ -0,0 +1,11 @@
+// vite.config.ts
+import { defineConfig } from "file:///E:/Note/Note/node_modules/vite/dist/node/index.js";
+import vue from "file:///E:/Note/Note/node_modules/@vitejs/plugin-vue/dist/index.mjs";
+import vueJsx from "file:///E:/Note/Note/node_modules/@vitejs/plugin-vue-jsx/dist/index.mjs";
+var vite_config_default = defineConfig({
+  plugins: [vueJsx(), vue()]
+});
+export {
+  vite_config_default as default
+};
+//# sourceMappingURL=data:application/json;base64,ewogICJ2ZXJzaW9uIjogMywKICAic291cmNlcyI6IFsidml0ZS5jb25maWcudHMiXSwKICAic291cmNlc0NvbnRlbnQiOiBbImNvbnN0IF9fdml0ZV9pbmplY3RlZF9vcmlnaW5hbF9kaXJuYW1lID0gXCJFOlxcXFxOb3RlXFxcXE5vdGVcIjtjb25zdCBfX3ZpdGVfaW5qZWN0ZWRfb3JpZ2luYWxfZmlsZW5hbWUgPSBcIkU6XFxcXE5vdGVcXFxcTm90ZVxcXFx2aXRlLmNvbmZpZy50c1wiO2NvbnN0IF9fdml0ZV9pbmplY3RlZF9vcmlnaW5hbF9pbXBvcnRfbWV0YV91cmwgPSBcImZpbGU6Ly8vRTovTm90ZS9Ob3RlL3ZpdGUuY29uZmlnLnRzXCI7aW1wb3J0IHsgZGVmaW5lQ29uZmlnIH0gZnJvbSAndml0ZSdcclxuaW1wb3J0IHZ1ZSBmcm9tICdAdml0ZWpzL3BsdWdpbi12dWUnXHJcbmltcG9ydCB2dWVKc3ggZnJvbSBcIkB2aXRlanMvcGx1Z2luLXZ1ZS1qc3hcIjtcclxuXHJcbi8vIGh0dHBzOi8vdml0ZWpzLmRldi9jb25maWcvXHJcbmV4cG9ydCBkZWZhdWx0IGRlZmluZUNvbmZpZyh7XHJcbiAgcGx1Z2luczogW3Z1ZUpzeCgpLCB2dWUoKV0sXHJcbn0pXHJcbiJdLAogICJtYXBwaW5ncyI6ICI7QUFBNE4sU0FBUyxvQkFBb0I7QUFDelAsT0FBTyxTQUFTO0FBQ2hCLE9BQU8sWUFBWTtBQUduQixJQUFPLHNCQUFRLGFBQWE7QUFBQSxFQUMxQixTQUFTLENBQUMsT0FBTyxHQUFHLElBQUksQ0FBQztBQUMzQixDQUFDOyIsCiAgIm5hbWVzIjogW10KfQo=
