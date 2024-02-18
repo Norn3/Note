542209f4b2e215c9d680a046d9ee5a453fc40d45
Norn3 <1430835578@qq.com>
2024-02-18 15:50:01 +0800

    build(install element plus, and remake searchframe): install element plus, and remake searchframe
    install element plus, and remake searchframe
    
    BREAKING CHANGE: install element plus

--- src/components/header/search/searchFrame.vue
+++ src/components/header/search/searchFrame.vue
@@ -1,14 +1,11 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { onMounted, ref } from 'vue';
 import { get } from '../../../axios/insatance';
 import { useRouter, useRoute } from 'vue-router';
 
 import './searchFrame.scss';
 
 let keyword = ref('');
-let search_word = '';
-const allowEmptyValueSearch = ref(false);
-let chosen_category = '';
 const router = useRouter();
 
 interface SourceItemObj {
@@ -16,8 +13,7 @@ interface SourceItemObj {
   name: string;
   index: number;
 }
-type SourceType = Array<string> | Array<SourceItemObj>;
-const mySource = ref<SourceType>([
+const mySource = ref<SourceItemObj[]>([
   {
     category: 'none',
     name: 'none',
@@ -25,14 +21,21 @@ const mySource = ref<SourceType>([
   },
 ]);
 
-const formatter = (item: SourceItemObj) => {
-  chosen_category = item.category;
-  return item.name;
+const querySearch = (queryString: string, cb: any) => {
+  let results: SourceItemObj[] = [];
+  searchFn(queryString)
+    .then((proxyArray) => {
+      results = Array.from(proxyArray);
+      cb(results);
+    })
+    .catch((error) => {
+      console.error('Promise rejected:', error);
+    });
 };
 
-//trem：input输入内容
-const searchFn = async (trem: string) => {
-  await get<any>(`/search/suggest?keywords=${trem}`)
+// queryString：input输入内容
+const searchFn = async (queryString: string) => {
+  await get<any>(`/search/suggest?keywords=${queryString}`)
     .then((response) => {
       // 处理返回的查询结果
       const result = response.result;
@@ -94,78 +97,38 @@ const searchFn = async (trem: string) => {
   return mySource.value;
 };
 
-const saveKeyword = (e: any) => {
-  if (e.keyCode != 13) {
-    search_word = keyword.value;
-  }
-};
-
-const jumpResult = (event: any) => {
-  keyword.value = search_word;
+const jumpResult = () => {
   router.push({
     name: 'search',
-    query: { keyword: search_word },
+    query: { keyword: keyword.value },
   });
 };
 
 // 点击搜索联想结果，打开页面
-const selectValue = (e: string) => {
-  // console.log(chosen_category);
-  // console.log(e);
-  get<any>(`/search/suggest?keywords=${e}`)
-    .then((song) => {
-      // 处理返回的用户数据
-      console.log(song);
-    })
-    .catch((error) => {
-      // 处理请求错误
-      console.log(error);
-    });
+const handleSelect = (item: string) => {
+  console.log(item);
 };
-
-const transInputFocusEmit = () => {
-  // console.log('transInputFocusEmit');
-};
-const position = ref(['bottom']);
 </script>
 
 <template>
   <div id="search" class="search">
-    <d-auto-complete
-      :delay="200"
-      :source="mySource"
-      :search-fn="searchFn"
-      :formatter="formatter"
-      is-searching
+    <el-autocomplete
       v-model="keyword"
-      @keyup="saveKeyword"
+      :fetch-suggestions="querySearch"
+      popper-class="my-autocomplete"
+      :trigger-on-focus="false"
+      clearable
+      placeholder="音乐/用户"
+      @select="handleSelect"
       @keyup.enter="jumpResult"
-      :allow-empty-value-search="allowEmptyValueSearch"
-      :select-value="selectValue"
-      :trans-input-focus-emit="transInputFocusEmit"
-      :position="position"
-      :width="200"
-      :append-to-body="false"
     >
-      <template #item="slotProps">
+      <template #default="{ item }">
         <div>
-          <span v-if="slotProps.item.index == 0">{{
-            slotProps.item.category + '\u3000'
-          }}</span>
+          <span v-if="item.index == 0">{{ item.category + '\u3000' }}</span>
           <span v-else>{{ '\u3000\u3000\u3000' }}</span>
-          <span>{{ slotProps.item.name }}</span>
+          <span>{{ item.name }}</span>
         </div>
       </template>
-      <template #nothing="slotProps">
-        <div>
-          {{ `没有匹配项: ${slotProps}` }}
-        </div>
-      </template>
-      <template #searching="slotProps">
-        <div>
-          {{ `searching: ${slotProps}` }}
-        </div>
-      </template>
-    </d-auto-complete>
+    </el-autocomplete>
   </div>
 </template>
--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,9 @@ import App from './App.vue'
 import router from "./router"
 import { createPinia } from 'pinia'
 import $ from 'jquery'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 
 const pinia = createPinia()
 
-createApp(App).use(router).use(pinia).use($).mount('#app')
+createApp(App).use(router).use(pinia).use($).use(ElementPlus, { size: 'small', zIndex: 3000 }).mount('#app')
