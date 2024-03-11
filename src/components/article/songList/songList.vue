88313e0f89f12a23acaf6f8feeb4a0856dd5c74d
Norn3 <1430835578@qq.com>
2024-03-11 14:43:16 +0800

    feat(add loading and scroll to load more in songlist): add loading and scroll to load more

--- src/components/article/songList/songList.vue
+++ src/components/article/songList/songList.vue
@@ -10,6 +10,13 @@
       <li id="album" class="album">专辑</li>
     </ul>
     <ul id="songs" class="songs"></ul>
+    <!-- <el-table v-if="already_getting" v-loading="already_getting" :data="tableData">
+      <el-table-column prop="title"/>
+    </el-table> -->
+    <el-text v-if="already_getting" v-loading="already_getting" class="loading"
+      >Loading...</el-text
+    >
+    <el-backtop :right="60" :bottom="60" />
   </div>
 </template>
 <style lang="scss"></style>
@@ -27,41 +34,39 @@ const props = defineProps({
     type: String,
     default: 'playlist',
   },
-  targetId: Number,
+  target_id: Number,
 });
 
-let sId = 0;
-
-const jumpPage = () => {
-  console.log(router);
-
-  router.push({
-    name: 'Info',
-    query: { id: sId },
-  });
-};
+let current_song_id = 0;
+const already_getting = ref(false);
 
 // 根据资源类型， 决定获取数据的地址
+// 只要调整limit就可以控制每次请求的歌曲数量，offset会根据列表项的渲染，所引起的current_song_id的自增，自动变化
 const address = (type: string): string => {
   let addr = '';
   switch (type) {
     case 'playlist':
-      addr = `/playlist/track/all?id=${props.targetId}`;
+      addr = `/playlist/track/all?id=${props.target_id}&limit=50&offset=${current_song_id}`;
       break;
     case 'album':
-      addr = `/album?id=${props.targetId}`;
+      addr = `/album?id=${props.target_id}&limit=50&offset=${current_song_id}`;
       break;
   }
   return addr;
 };
 
-// 获取包含的所有歌曲
+const firstGetSongs = async () => {
+  const $ul = $('#songList').find('#songs');
+  $ul.empty();
+  current_song_id = 0;
+  await getSongs();
+};
+
+// 获取歌单中的歌曲
 const getSongs = async () => {
   await get<any>(`${address(props.type)}`)
     .then((response) => {
       const $ul = $('#songList').find('#songs');
-      $ul.empty();
-      let id = 1;
       response.songs.forEach((song: any) => {
         const $li = $('<li>');
         $li.addClass('song_item');
@@ -81,7 +86,7 @@ const getSongs = async () => {
         render(
           h(SongListItem, {
             type: props.type,
-            listId: id++,
+            listId: ++current_song_id,
             songId: song.id,
             name: song.name,
             durationTime: song.dt,
@@ -100,20 +105,69 @@ const getSongs = async () => {
     });
 };
 
+// type ThrottledFunction = (...args: any[]) => void;
+// 节流方法, 防止达到可以请求数据的位置（already_getting == true）之前，onBottom被频繁调用，频繁获取高度数据
+const throttle = (func: () => void, delay: number) => {
+  let timer: any = null;
+  return (...args: any) => {
+    if (!timer) {
+      timer = setTimeout(() => {
+        func.apply(this);
+        timer = null;
+      }, delay);
+    }
+  };
+};
+// 触底相应函数
+const isReachBottom = (): boolean => {
+  // 获取可视高度（不包含滚动条宽度）
+  let clientHeight = $(window).height();
+  // 获取滚动高度
+  let scrollTop = $(window).scrollTop();
+  // 滚动条高度
+  let scrollHeight = $(document).height();
+
+  // 当窗口还差100px就到达页面底部时（因为有小数，所以触底时clientHeight + scrollTop = scrollHeight - 1）
+  if (
+    clientHeight &&
+    scrollTop &&
+    scrollHeight &&
+    clientHeight + scrollTop >= scrollHeight - 501
+  )
+    return true;
+  else return false;
+};
+const onScroll = async () => {
+  if (!already_getting.value) {
+    if (isReachBottom()) {
+      already_getting.value = true;
+      // 再请求下一批歌曲
+      await getSongs();
+      already_getting.value = false;
+    }
+  }
+};
+// 触底函数
+const onReachBottom = () => {
+  window.addEventListener('scroll', throttle(onScroll, 500));
+};
+
 onMounted(async () => {
   // 等待页面加载结束，再调用createItem创建列表项
   await nextTick();
   if (props.type != 'song') {
     getSongs();
   }
+  onReachBottom();
 });
 
 // 当页面的路由改变，马上重新获取信息
 watch(
-  () => props.targetId,
+  () => props.target_id,
   (newId, oldId) => {
     console.log(newId, oldId);
-    getSongs();
+    window.removeEventListener('scroll', throttle(onScroll, 500));
+    firstGetSongs();
   }
 );
 </script>
