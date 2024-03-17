849685af4f05e657af897d804412e0d330a612da
Norn3 <1430835578@qq.com>
2024-03-17 13:55:07 +0800

    refactor(small changes of style in playlist and sidebar): small changes of style

--- src/components/article/PlayList/PlayList.vue
+++ src/components/article/PlayList/PlayList.vue
@@ -1,8 +1,36 @@
+<!-- eslint-disable vue/no-useless-template-attributes -->
+<template>
+  <div id="playlist" class="play-list-container" :info="id" @click="jumpPage()">
+    <div
+      id="image"
+      class="image"
+      :style="{ 'background-image': `url(${imgUrl})` }"
+    >
+      <div id="mask" class="mask">
+        <div id="playCount" class="play-count">
+          <div id="icon" class="icon"></div>
+          <div id="number" class="number">
+            {{ processPlayCount(props.playCount) }}
+          </div>
+        </div>
+        <div id="playButton" class="play-button" @click.stop="playlist"></div>
+      </div>
+    </div>
+    <div id="description" class="description">
+      <a href="" id="title" class="title">{{ title }}</a>
+      <div id="creatorTitle" class="creatorTitle" v-if="showCreator">
+        by<a href="" id="creator" class="creator">{{ creator }}</a>
+      </div>
+    </div>
+  </div>
+</template>
+<style lang="scss"></style>
 <script setup lang="ts">
 import $, { event } from 'jquery';
 import { onMounted, nextTick } from 'vue';
 import router from '../../../router/index';
 // import { get } from '../../../axios/insatance';
+
 import processPlayCount from '../../../util/processPlayCount';
 
 import { useCurrentPlayingListStore } from '../../../stores/currentPlayingList';
@@ -41,6 +69,7 @@ const jumpPage = () => {
   });
 };
 
+// TODO：点击播放列表，没有loading态而且过程很长
 const playlist = async () => {
   await listStore.changeList('playlist', props.id as string);
   listStore.resetCurSong();
@@ -51,30 +80,3 @@ onMounted(async () => {
   await nextTick();
 });
 </script>
-<!-- eslint-disable vue/no-useless-template-attributes -->
-<template>
-  <div id="playlist" class="play-list-container" :info="id" @click="jumpPage()">
-    <div
-      id="image"
-      class="image"
-      :style="{ 'background-image': `url(${imgUrl})` }"
-    >
-      <div id="mask" class="mask">
-        <div id="playCount" class="play-count">
-          <div id="icon" class="icon"></div>
-          <div id="number" class="number">
-            {{ processPlayCount(props.playCount) }}
-          </div>
-        </div>
-        <div id="playButton" class="play-button" @click.stop="playlist"></div>
-      </div>
-    </div>
-    <div id="description" class="description">
-      <a href="" id="title" class="title">{{ title }}</a>
-      <div id="creatorTitle" class="creatorTitle" v-if="showCreator">
-        by<a href="" id="creator" class="creator">{{ creator }}</a>
-      </div>
-    </div>
-  </div>
-</template>
-<style lang="scss"></style>
