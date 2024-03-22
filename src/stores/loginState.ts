4f2c3514785e9c2b0e049af3d5b9fcc01e20c85e
Norn3 <1430835578@qq.com>
2024-03-22 16:28:43 +0800

    feat(improve loginstate): improve method and attribute
    remove initializeStore method, add getLoginStatus method for App.vue to initialize data when page
    refresh, update processLogin to get user's data when login, update processLogout for real logout,
    update requestUserProfile to return user's data.
    
    BREAKING CHANGE: remove initializeStore method, add getLoginStatus method for App.vue to initialize
    data when page refresh, update processLogin to get user's data when login, update processLogout for
    real logout, update requestUserProfile to return user's data.

--- src/stores/loginState.ts
+++ src/stores/loginState.ts
@@ -6,20 +6,27 @@ import { get } from '../axios/insatance';
 export const useLoginStateStore = defineStore('loginState', () => {
     
     const already_login = ref(false);
-    const cookie = ref<string | null>('');
     // 此处可以优化为，用一个数据类存储需要的属性，然后外部读取时调用该数据类的实例进行属性访问
     const profile = ref<any>('');
 
     // 创建实例用于一次更改多个属性而只触发一次subscribe
     const store = useLoginStateStore();
 
-    // 打开页面时将localStroage中的数据存入store中
-    const initializeStore = () => {
-        if(localStorage.getItem('currentCookie') != null) {
-            already_login.value = true;
-            cookie.value = localStorage.getItem('currentCookie');
-            requsetUserProfile();
-        }
+    // 获取登录状态
+    const getLoginStatus = () => {
+        // 加时间戳防止接口数据缓存
+        get<any>(`/login/status?timestamp=${Date.now()}`)
+        .then(async (response) => {
+            // 先获取到用户数据，最后再变更登录状态，以免其他组件watch被触发时用户数据仍为空
+            profile.value = await requsetUserProfile();
+            // 如果已登录，将登录状态改为true，否则改为false
+            already_login.value = response.data.account.status == -10 ? false : true;
+        })
+        .catch((error) => {
+            // 处理请求错误
+            console.log('获取登录状态失败');
+            console.log(error);
+        });
     }
 
     // 获取登录状态
@@ -32,29 +39,45 @@ export const useLoginStateStore = defineStore('loginState', () => {
     }
 
     // 处理登录
-    const processLogin = async (newCookie: string) => {
+    const processLogin = async () => {
+        profile.value = await requsetUserProfile();
         already_login.value = true;
-        cookie.value = newCookie;
-        localStorage.setItem('currentCookie', newCookie)
-        requsetUserProfile();
+
     }
 
     // 处理登出
     const processLogout = async () => {
+        // 加时间戳防止缓存
+        await get<any>(`/logout?timestamp=${Date.now()}`)
+            .then((response) => {
+                console.log(response);
+                console.log('退出登录成功');
+            })
+            .catch((error) => {
+                // 处理请求错误
+                console.log('退出登录失败');
+                console.log(error);
+            });
         already_login.value = false;
-        cookie.value = '';
-        localStorage.removeItem('currentCookie');
     }
 
 
     // 请求用户信息
-    const requsetUserProfile = () => {
-        get<any>('/user/account')
+    const requsetUserProfile = async (): Promise<any> => {
+        let profile = null;
+        await get<any>('/user/account')
         .then((response) => {
             console.log(response);
-            profile.value = response.profile;
+            profile = response.profile;
         })
+        .catch((error) => {
+            // 处理请求错误
+            console.log('请求失败');
+            console.log(error);
+          });
+        return profile;
     }
 
-    return {already_login, getLoginState, getProfile, processLogin, processLogout, initializeStore}
+
+    return {already_login, getLoginState, getProfile, processLogin, processLogout, getLoginStatus}
 })
