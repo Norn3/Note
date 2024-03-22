7f824a13171705449ce45c4a56e471134e530361
Norn3 <1430835578@qq.com>
2024-03-22 17:07:41 +0800

    feat(improve mysongvie): now mysongview carry the task that jump to myplaylistinfo
    when click headernav item, will jump to mySongView first, then mySongView will render sidebar and
    jump to myPlayListInfoView base on already_login

--- src/views/mySong/MySongView.vue
+++ src/views/mySong/MySongView.vue
@@ -74,18 +74,34 @@ section {
 
 <script setup lang="ts">
 import $ from 'jquery';
-import { reactive, onBeforeMount } from 'vue';
+import { reactive, onBeforeMount, watch } from 'vue';
 import { get } from '../../axios/insatance';
 
 import PlaylistItemClass from '../../class/PlaylistItemClass';
 
+import { useLoginStateStore } from '../../stores/loginState';
+
 import SideBar from '../../components/article/sideBar/sideBar.vue';
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+
+const loginStore = useLoginStateStore();
+
+let userId = loginStore.getLoginState() ? loginStore.getProfile().userId : '1';
+
+// TODO: 未登录应该不允许跳转，此页面要改
 
 let listItem: Array<PlaylistItemClass> = reactive([]);
+
 const getSideBar = () => {
-  get<any>(`/user/playlist?uid=${uid}&limit=100`)
+  console.log(userId);
+
+  get<any>(`/user/playlist?uid=${userId}&limit=100`)
     .then((response) => {
       console.log(response);
+      // 先清空listItem
+      listItem.splice(0, listItem.length);
       response.playlist.forEach((element: PlaylistItemClass) => {
         listItem.push(element);
       });
@@ -97,9 +113,46 @@ const getSideBar = () => {
     });
 };
 
-let uid = 571024254;
+// 获取“我的音乐”页面的首个歌单
+const getPid = async (): Promise<number> => {
+  let pid = 1;
+  await get<any>(`/user/playlist?uid=${userId}`)
+    .then((response) => {
+      pid = response.playlist[0].id;
+    })
+    .catch((error) => {
+      // 处理请求错误
+      console.log('获取首个歌单id失败');
+      console.log(error);
+    });
+  return pid;
+};
 
-onBeforeMount(() => {
+// 根据获取到的pid跳转到myPlaylistInfo
+const jumpPage = (pid: number) => {
+  router.push({ name: 'myPlaylistInfo', query: { id: pid } });
+  sessionStorage.setItem('lastPathName', 'myPlaylistInfo');
+  sessionStorage.setItem('lastPathQuery', JSON.stringify({ id: pid }));
+};
+
+// 渲染页面
+const renderPage = async () => {
   getSideBar();
+  let pid = await getPid();
+  jumpPage(pid);
+};
+
+// 如果在MySongView里执行了刷新操作，则重新获取userId并渲染页面
+watch(
+  () => loginStore.already_login,
+  (newValue, oldValue) => {
+    userId = newValue ? loginStore.getProfile().userId : '1';
+    renderPage();
+  }
+);
+
+// 根据点击“我的音乐”那一刻的userId渲染页面
+onBeforeMount(() => {
+  renderPage();
 });
 </script>
