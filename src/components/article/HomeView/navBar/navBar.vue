00a19c644516577d7ce6b06e19702b9e10a5e8f8
Norn3 <1430835578@qq.com>
2024-03-22 16:52:40 +0800

    feat(improve navbar): improve navBar to watch route.name, change checked item of navBar
    improve navBar to watch route.name, change checked item of navBar

--- src/components/article/HomeView/navBar/navBar.vue
+++ src/components/article/HomeView/navBar/navBar.vue
@@ -13,46 +13,9 @@
 </template>
 <script setup lang="ts">
 import $ from 'jquery';
-import { onMounted } from 'vue';
+import { onMounted, watch } from 'vue';
 import { useRouter, useRoute } from 'vue-router';
 import './navBar.scss';
-
-// // 通过defineProps接收父组件的值
-// const father = defineProps({
-//   // 接收传值   此处的itemNum就是父组件的自定义名称
-//   itemNum: {
-//     type: Number, // 数据类型
-//     default: 0, // 未传值时的默认值
-//   },
-//   navItem: {
-//     type: Array,
-//     default: () => [
-//       'Item11568468796/8746844654134854135',
-//       'Item2',
-//       'Item3',
-//       'Item4',
-//     ],
-//   },
-// });
-// const navItem = reactive(father.navItem);
-
-// const createItem = () => {
-//   // 使用 $() 将目标元素包装为 jQuery 对象
-//   const $nav = $('.nav');
-
-//   navItem.forEach((element) => {
-//     // 创建新的子元素并设置其内容
-//     const $li = $('<li>').text(String(element));
-//     $li.addClass('navItem');
-//     // 将新创建的子元素添加为目标元素的子节点
-//     $nav.append($li);
-//   });
-// };
-
-// onMounted(async () => {
-//   await nextTick();
-//   createItem();
-// });
 
 // 创建导航栏内容
 const navItem = [
@@ -64,11 +27,27 @@ const navItem = [
 // 选择导航栏中的每一项，都会将改变router到对应路径，并且更改选中样式
 const router = useRouter();
 const route = useRoute();
-const jumpPage = (address: string, id: string) => {
-  // 选中对应项
+
+// 监听路由变化
+watch(
+  () => route.name,
+  (newValue) => {
+    selectItem(newValue as string);
+  }
+);
+
+// 在导航栏中查找传入的路径的对应项，若有则选中该项
+const selectItem = (select_address: string) => {
   $('.navItem').removeClass('check');
-  $(id)[0].classList.add('check');
-  // 根据地址，在sessionStorage中更改最后所在路径，并进行跳转
+  console.log(select_address);
+
+  let check_item = navItem.findIndex((item) => item.address == select_address);
+  // 给对应项添加选中类名
+  $(`#li${check_item}`).addClass('check');
+};
+
+// 根据地址，在sessionStorage中更改最后所在路径，并进行跳转
+const jumpPage = (address: string, id: string) => {
   sessionStorage.setItem('lastPathName', address);
 
   if (address == 'playlist') {
@@ -92,20 +71,15 @@ const jumpPage = (address: string, id: string) => {
   }
 };
 
-// 加载时，选中第一项，用onMounted才能给选中项添加样式
+// 加载时，根据路由，给对应项添加选中状态
 onMounted(() => {
   // 上次最后到哪
   let path_name = sessionStorage.getItem('lastPathName') as string;
-
-  // 选中对应项
-  let check_item = navItem.findIndex((item) => item.address == path_name);
-
   // 如果没有找到对应路径，比如初次打开，则打开推荐页
   if (route.name == 'home' || path_name == null) {
-    check_item = 0;
+    path_name = 'recommend';
   }
-  // 给对应项添加选中类名
-  $(`#li${check_item}`).addClass('check');
+  selectItem(path_name);
 });
 </script>
 
