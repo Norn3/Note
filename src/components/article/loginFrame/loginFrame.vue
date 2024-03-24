3a6576580ccad7be9b46c9d8916f1a73f65b2b9a
Norn3 <1430835578@qq.com>
2024-03-24 15:16:04 +0800

    feat(add passwordloginform and add rules on it): add passwordLoginForm and add rules on it
    make passwordLoginForm independent of loginFrame, add rules to validate input

--- src/components/article/loginFrame/loginFrame.vue
+++ src/components/article/loginFrame/loginFrame.vue
@@ -11,32 +11,7 @@
           ></div>
         </div>
         <div id="frameContent" class="frame_content">
-          <el-form
-            label-position="left"
-            label-width="auto"
-            :model="phoneAndPassword"
-            size="large"
-            class="password_login_form"
-          >
-            <el-form-item>
-              <el-input
-                v-model="phoneAndPassword.phone"
-                placeholder="请输入手机号"
-              />
-            </el-form-item>
-            <el-form-item>
-              <el-input
-                v-model="phoneAndPassword.password"
-                placeholder="请输入密码"
-                show-password
-              />
-            </el-form-item>
-            <el-form-item>
-              <el-button class="submit_button" @click="onSubmit"
-                >登录</el-button
-              >
-            </el-form-item>
-          </el-form>
+          <password-login-frame></password-login-frame>
         </div>
       </div>
       <!-- <img src="https://p5.music.126.net/obj/wo3DlcOGw6DClTvDisK1/9643571155/525c/faac/2dc6/fe695c03c7c358ddaa4651736b26a55f.png" alt="提示图片 ············"> -->
@@ -53,32 +28,14 @@ import { useRoute, useRouter } from 'vue-router';
 
 import { useLoginStateStore } from '../../../stores/loginState';
 
+import passwordLoginFrame from './passwordLoginForm/passwordLoginForm.vue';
+
 import './loginFrame.scss';
 
 const router = useRouter();
 const route = useRoute();
 
 const loginStore = useLoginStateStore();
-
-const phoneAndPassword = reactive({
-  phone: '',
-  password: '',
-});
-
-const onSubmit = async () => {
-  console.log(phoneAndPassword);
-  const login_result = await loginStore.processLogin(
-    phoneAndPassword.phone,
-    phoneAndPassword.password
-  );
-  if (login_result) {
-    loginStore.hideLoginEntry();
-    router.push({ name: 'recommend' });
-    sessionStorage.setItem('lastPathName', 'recommend');
-  } else {
-    alert('账号或密码不正确');
-  }
-};
 
 const hideLoginFrame = () => {
   loginStore.hideLoginEntry();
---
+++ src/components/article/loginFrame/passwordLoginForm/passwordLoginForm.vue
@@ -0,0 +1,113 @@
+<template>
+  <el-form
+    ref="passwordLoginFormRef"
+    :model="phoneAndPassword"
+    :rules="rules"
+    label-position="left"
+    label-width="auto"
+    size="large"
+    class="password_login_form"
+  >
+    <el-form-item prop="phone">
+      <el-input
+        v-model="phoneAndPassword.phone"
+        placeholder="请输入手机号"
+        clearable
+      />
+    </el-form-item>
+    <el-form-item prop="password">
+      <el-input
+        v-model="phoneAndPassword.password"
+        placeholder="请输入密码"
+        type="password"
+        clearable
+        @keyup.enter="onSubmit(passwordLoginFormRef)"
+      />
+    </el-form-item>
+    <el-form-item>
+      <el-button class="submit_button" @click="onSubmit(passwordLoginFormRef)"
+        >登录</el-button
+      >
+    </el-form-item>
+  </el-form>
+</template>
+
+<style lang="scss" scoped></style>
+
+<script setup lang="ts">
+import { onMounted, ref, nextTick, reactive, watch } from 'vue';
+import { useRoute, useRouter } from 'vue-router';
+
+import { useLoginStateStore } from '../../../../stores/loginState';
+
+import './passwordLoginForm.scss';
+import type { FormInstance, FormRules } from 'element-plus';
+
+const router = useRouter();
+const route = useRoute();
+
+const loginStore = useLoginStateStore();
+
+const passwordLoginFormRef = ref<FormInstance>();
+
+const validatePhone = (rule: any, value: any, callback: any) => {
+  if (value === '') {
+    callback(new Error('请输入手机号！'));
+  } else {
+    const phone_reg =
+      /^(13[0-9]|14[01456879]|15[0-35-9]|16[2567]|17[0-8]|18[0-9]|19[0-35-9])\d{8}$/;
+    if (!phone_reg.test(value)) {
+      callback(new Error('请输入正确的手机号！'));
+    }
+    callback();
+  }
+};
+const validatePassword = (rule: any, value: any, callback: any) => {
+  if (value === '') {
+    callback(new Error('请输入密码！'));
+  } else {
+    callback();
+  }
+};
+
+const phoneAndPassword = reactive({
+  phone: '',
+  password: '',
+});
+
+const rules = reactive<FormRules<typeof phoneAndPassword>>({
+  phone: [{ validator: validatePhone, trigger: 'blur' }],
+  password: [{ validator: validatePassword, trigger: 'blur' }],
+});
+
+const onSubmit = (formInstance: FormInstance | undefined) => {
+  if (!formInstance) return;
+  formInstance.validate(async (valid) => {
+    if (!valid) {
+      return false;
+    } else {
+      const login_result = await loginStore.processLogin(
+        phoneAndPassword.phone,
+        phoneAndPassword.password
+      );
+      // 登录成功
+      if (login_result) {
+        // 隐藏登录框
+        // loginStore.hideLoginEntry();
+        // 清空登录框内的账号密码
+        if (formInstance) {
+          formInstance.resetFields();
+        }
+        // 跳转到首页
+        router.push({ name: 'recommend' });
+        sessionStorage.setItem('lastPathName', 'recommend');
+      }
+      // 登录失败
+      else {
+        // alert('账号或密码不正确');
+        return false;
+      }
+    }
+  });
+};
+</script>
