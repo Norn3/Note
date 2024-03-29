51a881fc249281a1aae3510d515d938e98fdd61d
Norn3 <1430835578@qq.com>
2024-03-29 13:55:06 +0800

    feat(add moment.js, fix install vue-eslint-parser): add moment.js, fix install vue-eslint-parser

--- src/views/account/accountHomeView.vue
+++ src/views/account/accountHomeView.vue
@@ -1,19 +1,41 @@
 <template>
   <div class="account_view">
     <main>
-      <section v-if="createList.length || faroviteList.length">
-        <playlistList
-          v-if="createList.length"
-          type="myCreate"
-          :playlists="createList"
-        ></playlistList>
-        <playlistList
-          v-if="faroviteList.length"
-          type="myFavorite"
-          :playlists="faroviteList"
-        ></playlistList>
+      <section>
+        <div id="userInfoContainer" class="user_info_container">
+          <div id="userAvatar" class="user_avatar"></div>
+          <div id="userName" class="user_name">{{ userName }}</div>
+          <div id="userLevel" class="user_level">{{ 'Lv' + userLevel }}</div>
+          <div id="userGender" class="user_gender">{{ userGender }}</div>
+          <div id="userAddress" class="user_address">
+            {{ userProvince + '-' + userCity }}
+          </div>
+          <div id="userAge" class="user_age">{{ userAge }}</div>
+          <div id="userCreateDays" class="user_create_days">
+            {{ '这是您加入我们的第' + userCreateDays + '天' }}
+          </div>
+          <div id="userListenedSongs" class="user_listened_songs">
+            {{ '您已听歌' + userListenedSongs + '首' }}
+          </div>
+        </div>
+        <div
+          id="personalPlaylist"
+          class="personal_playlist"
+          v-if="createList.length || faroviteList.length"
+        >
+          <playlistList
+            v-if="createList.length"
+            type="myCreate"
+            :playlists="createList"
+          ></playlistList>
+          <playlistList
+            v-if="faroviteList.length"
+            type="myFavorite"
+            :playlists="faroviteList"
+          ></playlistList>
+          <noData v-else></noData>
+        </div>
       </section>
-      <noData v-else></noData>
     </main>
   </div>
 </template>
@@ -40,35 +62,28 @@ aside {
 }
 section {
   position: relative;
+  top: 10vh;
   height: 100%;
   min-height: 92vh;
   width: 100%;
   margin: 0 auto;
-  .playlistTitle {
-    margin: 20px;
-    border-bottom: 2px solid $audioCurrentState;
-    display: flex;
-    h1 {
-      position: relative;
-      display: inline-block;
-      margin: 10px;
-      margin-left: 40px;
-      font-weight: 500;
+
+  .user_info_container {
+    display: grid;
+    padding: 0 5vw;
+
+    .user_avatar {
+      width: 10vw;
+      height: 21.8vh;
+      background-position: center;
+      background-repeat: no-repeat;
+      background-size: contain;
     }
-  }
-  .playlists {
-    box-sizing: border-box;
-    height: auto;
-    width: 100%;
-    padding: 0 2%;
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: left;
-    content-visibility: auto;
   }
 }
 </style>
 <script setup lang="ts">
+// TODO：后续把这个页面改成个人主页，accountHomeView内用vue router指向这一页，用userId作为参数传入
 // import { useRoute } from 'vue-router'
 import $ from 'jquery';
 import {
@@ -82,6 +97,7 @@ import {
 } from 'vue';
 import { useRouter } from 'vue-router';
 import { get } from '../../axios/insatance';
+import moment from 'moment';
 
 import { useLoginStateStore } from '../../stores/loginState';
 
@@ -90,6 +106,15 @@ import noData from '../../components/article/noData/noData.vue';
 
 const loginState = useLoginStateStore();
 const uid = loginState.getProfile().userId;
+
+const userName = ref('');
+const userLevel = ref('');
+const userGender = ref('');
+const userProvince = ref('');
+const userCity = ref('');
+const userAge = ref('');
+const userCreateDays = ref('');
+const userListenedSongs = ref('');
 
 const createList = ref([]);
 const faroviteList = ref([]);
@@ -114,7 +139,38 @@ const getPlaylists = (uid: string) => {
     });
 };
 
+const getInfo = (uid: string) => {
+  get<any>(`/user/detail?uid=${uid}`)
+    .then((response) => {
+      console.log(response);
+      $('#userAvatar').css(
+        'background-image',
+        `url(${response.profile.avatarUrl})`
+      );
+      userName.value = response.profile.nickname;
+      userLevel.value = response.level;
+      if (response.profile.gender == 0) {
+        userGender.value = '保密';
+      } else {
+        userGender.value = response.profile.gender == 1 ? '男' : '女';
+      }
+      userProvince.value = response.profile.province;
+      userCity.value = response.profile.city;
+      userAge.value = String(
+        moment(new Date(response.profile.birthday)).format('YYYY年MM月DD日')
+      );
+      userCreateDays.value = response.createDays;
+      userListenedSongs.value = response.listenSongs;
+    })
+    .catch((error) => {
+      // 处理请求错误
+      console.log('请求失败');
+      console.log(error);
+    });
+};
+
 onMounted(() => {
+  getInfo(uid);
   getPlaylists(uid);
 });
 </script>
